bcc17209f31108c39713dcc33fd344711d442067
MarioC17 <mariocalderon00@gmail.com>
2021-06-02 23:25:55 -0600

    Add return and rental transaction screens

---
+++ CarRental/ReturnTransactionForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CarRental
+{
+    public partial class ReturnTransactionForm : Form
+    {
+        public ReturnTransactionForm()
+        {
+            InitializeComponent();
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
