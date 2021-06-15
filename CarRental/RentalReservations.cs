5f19ce105371ae7528d323c290a24874faa47749
josephlatina <josephlatina93@gmail.com>
2021-06-15 13:26:45 -0600

    linked RentalReservation and connection comboboxes to database

--- CarRental/RentalReservations.cs
+++ CarRental/RentalReservations.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.SqlClient;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,9 +11,65 @@ namespace CarRental
 {
     public partial class RentalReservations : Form
     {
+        public SqlConnection myConnection;
+        public SqlCommand myCommand;
+        public SqlDataReader myReader;
+
         public RentalReservations()
         {
             InitializeComponent();
+
+            SqlConnection myConnection = new SqlConnection("user id=admin291;" +
+                                        "password=cmpt291;" +
+                                        "server=localhost;" +
+                                        "database=CarRental; " +
+                                        "connection timeout=30");
+            //Connect to Database
+            try
+            {
+                myConnection.Open();
+                myCommand = new SqlCommand();
+                myCommand.Connection = myConnection;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString(), "Error");
+                this.Close();
+            }
+
+            //Populate the items in the following comboboxes
+            try
+            {
+                //Retrieving customers
+                myCommand.CommandText = "select * from Customer";
+                myReader = myCommand.ExecuteReader();
+                while (myReader.Read())
+                {
+                    customerList.Items.Add(myReader["customerID"].ToString() + " - " + myReader["fName"].ToString() + " " + myReader["lName"].ToString());
+                }
+                myReader.Close();
+                //Retrieving employees
+                myCommand.CommandText = "select * from Employee";
+                myReader = myCommand.ExecuteReader();
+                while (myReader.Read())
+                {
+                    employeeList.Items.Add(myReader["empID"].ToString() + " - " + myReader["fName"].ToString() + " " + myReader["lName"].ToString());
+                }
+                myReader.Close();
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString(), "Error");
+                this.Close();
+            }
+        }
+
+        private void backButton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            EmployeeCars r1 = new EmployeeCars();
+            r1.ShowDialog();
         }
     }
 }
